4a44b3ab3db98265774234d46ef31f731083935c
DewanggaJunior <glendyjunior7@gmail.com>
2026-05-16 17:34:21 +0700

    update backend, dan rubah admin page sedikit

--- src/main/java/com/electro/repository/DeviceRepository.java
+++ src/main/java/com/electro/repository/DeviceRepository.java
@@ -3,11 +3,11 @@ package com.electro.repository;
 import com.electro.model.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
-    List<Device> findByHarga(double harga);
+    List<Device> findByNama(String nama);
     List<Device> findAllByOrderByHargaAsc();
+    List<Device> findAllByOrderByHargaDesc();
 }
