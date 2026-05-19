b2ed7838d8d2c809d3f5ebc8b9e4d413432796e8
DewanggaJunior <glendyjunior7@gmail.com>
2026-05-20 01:41:51 +0700

    Tambah model: Tv, Ac

---
+++ src/main/java/com/electro/model/Ac.java
@@ -0,0 +1,13 @@
+package com.electro.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@DiscriminatorValue("Ac")
+public class Ac extends Electronic {
+    public Ac() {}
+    public Ac(String nama, double harga) { super(nama, harga); }
+
+    @Override
+    public String getKategori() { return "Ac"; }
+}
---
+++ src/main/java/com/electro/model/Tv.java
@@ -0,0 +1,13 @@
+package com.electro.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@DiscriminatorValue("Tv")
+public class Tv extends Electronic {
+    public Tv() {}
+    public Tv(String nama, double harga) { super(nama, harga); }
+
+    @Override
+    public String getKategori() { return "Tv"; }
+}
