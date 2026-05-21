16095a712d59e24d80c4629f16df3576a4861dcb
DewanggaJunior <glendyjunior7@gmail.com>
2026-05-21 19:11:38 +0700

    Update LoginController

--- src/main/java/com/electro/controller/LoginController.java
+++ src/main/java/com/electro/controller/LoginController.java
@@ -2,12 +2,18 @@ package com.electro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(required = false) String error, Model model){
+
+        if(error != null){
+            model.addAttribute("error", "Username atau password salah!");
+        }
         return "login"; 
     }
-    
 }
